6adf65d80d09e55356348ffda0c30c548e458cfe
ABOBA <aboba@gmail.com>
2022-07-08 10:33:37 +0300

    Написаны тесты для метода remove.

--- src/test/java/LInkedTreeTest.java
+++ src/test/java/LInkedTreeTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class LInkedTreeTest {
 	@Test
@@ -36,5 +37,65 @@ public class LInkedTreeTest {
 				"node\n" +
 				"\tnode1\n";
 		assertEquals(trueResult, tree.toString());
+	}
+
+	@Test
+	void removeNode() {
+		LinkedTree tree = new LinkedTree("node");
+		tree.add("node1", "node");
+		tree.add("node2", "node");
+		tree.add("node3", "node");
+		// Сохраняем дерево до удаления.
+		String firstTree = tree.toString();
+		// Теперь добавляем узел и сохраняем дерево.
+		tree.add("node11", "node1");
+		String secondTree = tree.toString();
+		assertNotEquals(firstTree, secondTree);
+		// Удаляем последний добавленный узел.
+		tree.remove("node11");
+		assertEquals(firstTree, tree.toString());
+	}
+
+	@Test
+	void removeSubtree() {
+		LinkedTree tree = new LinkedTree("node");
+		tree.add("node1", "node");
+		tree.add("node2", "node");
+		tree.add("node3", "node");
+		tree.add("node11", "node1");
+		// В данный момент дерево имеет такую структуру.
+		String firstTree =
+				"node\n" +
+				"\tnode1\n" +
+				"\t\tnode11\n" +
+				"\tnode2\n" +
+				"\tnode3\n";
+		assertEquals(firstTree, tree.toString());
+		// Нам нужно сделать такую структуру.
+		String secondTree =
+				"node\n" +
+				"\tnode2\n" +
+				"\tnode3\n";
+		tree.remove("node1");
+		assertEquals(secondTree, tree.toString());
+	}
+
+	// Тестируем удаление несуществующего узла.
+	@Test
+	void removeNode1() {
+		LinkedTree tree = new LinkedTree("node");
+		tree.add("node1", "node");
+		tree.add("node2", "node");
+		tree.add("node3", "node");
+		tree.add("node11", "node1");
+		String firstTree =
+				"node\n" +
+				"\tnode1\n" +
+				"\t\tnode11\n" +
+				"\tnode2\n" +
+				"\tnode3\n";
+		assertEquals(firstTree, tree.toString());
+		tree.remove("folder");
+		assertEquals(firstTree, tree.toString());
 	}
 }
